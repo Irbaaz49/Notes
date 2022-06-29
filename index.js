0dbf2f2a133ed45f021f6c7039cbd97d9336644f
irbaaz <mwater196@gmail.com>
2022-06-29 12:58:41 +0530

    adding Funcationality

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-// #202124
-const addmore = document.getElementById('addmore');
+//getting add note button
+
+const addNote = document.getElementById('addmore');
 
 
-const addNote = (text = '')=>{
+//creating addNotes function
 
-const note = document.createElement('div')
-note.classList.add('note');
+const addNotes = (text = '') => {
 
-const htmlData = `<div class="btns">
+    //Add button 
+
+    //creating div elemnt through JS and adding 'notes' class to div element 
+
+    const notes = document.createElement('div');
+
+    notes.classList.add('notes');
+
+    const HtmlData = `<div class="btns">
 <button id="edit"><i class="fa-solid fa-pen-to-square"></i></button>
 <button id="delete"><i class="fa-solid fa-trash-can"></i></button>
 </div>
 <div id="divtext"></div>
-<textarea name="text" id="textarea" cols="30" rows="10"></textarea>`;
+<textarea name="text" id="textarea" cols="30" rows="10">`;
 
-note.insertAdjacentHTML('afterbegin',htmlData);
+    //inserting html data into the div element 
 
-
-const edit = note.querySelector('#edit');
-const DelteBtn = note.querySelector('#delete');
-const textarea = note.querySelector('#textarea');
-const main = note.querySelector('#divtxt');
+    notes.insertAdjacentHTML('afterbegin', HtmlData);
 
 
 
+    //appending the inserted html data and the div to the body
 
-//deleting the note
-DelteBtn.addEventListener('click',function(){
-    note.remove();
+    document.body.appendChild(notes);
+
+
+
+
+    //Delete Button
+
+    //Getting the delete btn from notes div  
+    const deleBtn = notes.querySelector('#delete');
+
+    // Adding event listener to btn and removing the child node from the body
+    deleBtn.addEventListener('click', () => {
+        document.body.removeChild(notes);
+    })
+
+
+    //Edit and save
+
+    //getting edit btn and textarea 
+
+    const editBtn = notes.querySelector('#edit');
+    const textArea  = notes.querySelector('#textarea');
+    const divText = notes.querySelector('#divtext')
+
+let change = '';
+textArea.addEventListener('change',(e)=>{
+change = e.target.value;
+})
+
+
+editBtn.addEventListener('click',()=>{
+    textArea.classList.toggle('hidden');
+    divText.innerHTML = change;
 })
 
 
 
 
-
-
-
-
-
-
-document.body.appendChild(note);
-//appends the node as the last child of a node
 }
-addmore.addEventListener('click', ()=>addNote());
+
+
+
+//Adding event listember to addnote
+addNote.addEventListener('click', () => addNotes())
